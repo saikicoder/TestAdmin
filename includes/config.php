d48a96750d8cdff2018cd84080e3d3ddc5c14b48
saikicoder <saikicoder@gmail.com>
2024-07-10 10:40:44 -0400

    Just updating

---
+++ includes/config.php
@@ -0,0 +1,18 @@
+<?php
+$host = '127.0.0.1';
+$dbname = 'admin_db';
+$username = 'root';
+$password = '';
+$port = '4306'; // Replace with your MySQL port number if different
+
+try {
+    $pdo = new PDO("mysql:host=$host;port=$port;dbname=$dbname", $username, $password, [
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION, // Enable exceptions for error handling
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC, // Fetch associative arrays by default
+        PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8mb4" // Optional: Set the character set
+    ]);
+
+    //echo "Connected successfully!";
+} catch (PDOException $e) {
+    die("Connection failed: " . $e->getMessage());
+}
