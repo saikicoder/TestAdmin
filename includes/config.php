5ec0c23cacc71d4dfe87dab723435efd91ebb6d0
saikicoder <saikicoder@gmail.com>
2024-07-22 09:31:23 -0400

    config file updated

--- includes/config.php
+++ includes/config.php
@@ -1,9 +1,9 @@
 <?php
 $host = 'localhost';
-$dbname = 'amritaent';
-$username = 'amritaent_admin';
-$password = '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';
-//$port = '4306'; // Replace with your MySQL port number if different
+$dbname = 'admin_db';
+$username = 'root';
+$password = '';
+$port = '4306'; // Replace with your MySQL port number if different
 
 try {
     $pdo = new PDO("mysql:host=$host;port=$port;dbname=$dbname", $username, $password, [
