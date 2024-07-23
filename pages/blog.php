5e54515979118bc429c206c4e8df6257c25417dc
saikicoder <saikicoder@gmail.com>
2024-07-23 12:50:55 -0400

    form submission updated

--- pages/blog.php
+++ pages/blog.php
@@ -101,8 +101,46 @@ if($_SERVER['REQUEST_METHOD'] === 'GET')
 }
 
 $title = "Blog";
+
+
 include_once("head_nav.php");
 ?>
+
+<style>
+/* Styles for the overlay */
+.overlay {
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-color: rgba(0, 0, 0, 0.5); /* Black background with opacity */
+    display: none; /* Initially hidden */
+    justify-content: center;
+    align-items: center;
+    z-index: 9999; /* Make sure it appears on top of other content */
+  }
+  
+  /* Spinner styles */
+  .spinner {
+    border: 16px solid #f3f3f3; /* Light grey */
+    border-top: 16px solid #3498db; /* Blue */
+    border-radius: 50%;
+    width: 120px;
+    height: 120px;
+    animation: spin 2s linear infinite;
+  }
+  
+  @keyframes spin {
+    0% { transform: rotate(0deg); }
+    100% { transform: rotate(360deg); }
+  }
+  
+  /* Hide the overlay initially */
+          body.loading .overlay {
+              display: flex;
+          }
+</style>
     <script>
         $(document).ready(function() {
             var id = "<?php echo $id; ?>";
@@ -200,7 +238,8 @@ include_once("head_nav.php");
             </div>
         </div>
     </div>
-                                    <!-- Overlay element with spinner -->
+    
+    <!-- Overlay element with spinner -->
     <div id="overlay" class="overlay">
         <div class="spinner"></div>
     </div>
